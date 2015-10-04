0d618cfdd175e2c75563cfac5dc271ab69f085d8
Victor Laya <viclaya1@hotmail.com>
2015-10-03 22:46:49 -0430

    database

--- sumipanlosllanos.sql
+++ sumipanlosllanos.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-07-2015 a las 04:33:25
+-- Tiempo de generación: 04-10-2015 a las 04:47:54
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.5.19
 
@@ -30,7 +30,14 @@ CREATE TABLE IF NOT EXISTS `administrador` (
 `id` int(11) NOT NULL,
   `usuario` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `clave` varchar(30) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `administrador`
+--
+
+INSERT INTO `administrador` (`id`, `usuario`, `clave`) VALUES
+(1, 'admin', '1234');
 
 -- --------------------------------------------------------
 
@@ -53,7 +60,15 @@ CREATE TABLE IF NOT EXISTS `orden` (
   `t2` int(14) NOT NULL,
   `fax` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
   `fecha_pedido` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `orden`
+--
+
+INSERT INTO `orden` (`id`, `nnegocio`, `propietario`, `rif`, `cedula`, `direccion`, `ciudad`, `estado`, `parroquia`, `cpostal`, `t1`, `t2`, `fax`, `fecha_pedido`) VALUES
+(1, 'VIKTTORLY', 'VICTOR LAYA', 'J-2131223-G', 25132279, 'Casa 23 Calle 4', 'Calaboxo', 'GuÃ¡rico', 'Miranda', 2312, 8712376, 2147483647, '8712376', '2015-09-27 22:32:04'),
+(8, 'CHOCOLATERA', 'BILLI WONCA', 'J-12312312-C', 123456, 'LA FABRICA', 'NUNCA JAMAS', 'PAIS DE LAS MARAVILLAS', 'ALICIA', 2312, 87654321, 12345678, 'HOLAMUNDO.JPG', '2015-09-28 00:11:41');
 
 -- --------------------------------------------------------
 
@@ -68,7 +83,14 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `nombres` text COLLATE utf8_spanish_ci NOT NULL,
   `apellidos` text COLLATE utf8_spanish_ci NOT NULL,
   `correo` varchar(50) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `usuario`, `clave`, `nombres`, `apellidos`, `correo`) VALUES
+(1, 'Vikttorly', 'VICTOR MANUEL', 'LAYA CASTILLO', '25132279', 'viclaya1@hotmail.com');
 
 --
 -- Índices para tablas volcadas
@@ -100,17 +122,17 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `administrador`
 --
 ALTER TABLE `administrador`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `orden`
 --
 ALTER TABLE `orden`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
